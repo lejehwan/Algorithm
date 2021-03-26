02ad276baa78511757da19fc687a43cbcda75867
lehehwan <rdsa257@naver.com>
2021-03-26 10:11:38 +0900

    Level1

---
+++ Algorithm/src/programmers/정수내림차순으로배치하기.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 정수내림차순으로배치하기 {
+}
+
+class 정수내림차순으로배치하기Solution{
+	public long solution(long n) {
+		String[] str = String.valueOf(n).split("");
+        Arrays.sort(str);
+        String temp = "";
+        for(int i = str.length-1; i >=0; i--){
+        	temp += str[i];
+        }
+        Long answer = Long.parseLong(temp);
+        return answer;
+	}
+}
