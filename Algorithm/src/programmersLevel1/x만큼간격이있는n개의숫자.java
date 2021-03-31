ee12760b1b22cce83ea48b092dd16f881a3eaa34
lehehwan <rdsa257@naver.com>
2021-03-31 20:13:51 +0900

    Level1

---
+++ Algorithm/src/programmersLevel1/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,15 @@
+package programmersLevel1;
+
+public class x만큼간격이있는n개의숫자 {
+
+}
+
+class x만큼간격이있는n개의숫자Solution{
+	public long[] solution(int x, int n) {
+		long[] answer = new long[n];
+		for (int i = 0; i < n; i++) {
+			answer[i] = (long)x*(i+1);
+		}
+		return answer;
+	}
+}
