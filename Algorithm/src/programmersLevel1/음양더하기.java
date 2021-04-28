c50b1cf0627b336a35c34e3c1f812c6e72f15174
lehehwan <rdsa257@naver.com>
2021-04-29 08:49:19 +0900

    Level1

---
+++ Algorithm/src/programmersLevel1/음양더하기.java
@@ -0,0 +1,19 @@
+package programmersLevel1;
+
+public class 음양더하기 {
+
+}
+
+class 음양더하기Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+			if (signs[i]) {
+				answer += absolutes[i];
+			} else {
+				answer -= absolutes[i];
+			}
+		}
+        return answer;
+    }
+}
