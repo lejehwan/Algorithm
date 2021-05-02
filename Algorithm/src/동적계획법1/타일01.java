77ca8adda82545be1f840e88e49337ffb55cc8f3
lehehwan <rdsa257@naver.com>
2021-05-02 11:51:21 +0900

    동적계획법

---
+++ Algorithm/src/동적계획법1/타일01.java
@@ -0,0 +1,40 @@
+package 동적계획법1;
+
+import java.util.Scanner;
+
+// bottom-up
+public class 타일01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] dp = new long[n + 1];
+		dp[0] = 1;
+		dp[1] = 2;
+		for (int i = 2; i < dp.length; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+		}
+		System.out.println(dp[n - 1]);
+		sc.close();
+	}
+}
+
+//// top-down
+//class 타일01{
+//	static long[] dp = new long[1000001];
+
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		dp[0] = 1;
+//		dp[1] = 2;
+//		tile(n);
+//		System.out.println(dp[n-1]);
+//		sc.close();
+//	}
+//	static long tile(int n) {
+//		if (dp[n] != 0) {
+//			return dp[n];
+//		}
+//		return dp[n] = (tile(n-1) + tile(n-2)) % 15746;
+//	}
+//}
---
+++ Algorithm/src/동적계획법1/파도반수열.java
@@ -0,0 +1,52 @@
+package 동적계획법1;
+
+import java.util.Scanner;
+
+// bottom-up
+public class 파도반수열 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long[] dp = new long[101];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		dp[4] = 2;
+		for (int i = 5; i < dp.length; i++) {
+			dp[i] = dp[i-1] + dp[i-5];
+		}
+		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			System.out.println(dp[m]);
+		}
+		sc.close();
+	}
+}
+
+//// top-down
+//class 파도반수열 {
+//	static long[] dp = new long[101];
+//
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		dp[1] = 1;
+//		dp[2] = 1;
+//		dp[3] = 1;
+//		dp[4] = 2;
+//		dp[5] = 2;
+//		for (int i = 0; i < n; i++) {
+//			int m = sc.nextInt();
+//			System.out.println(padovan(m));
+//		}
+//		sc.close();
+//	}
+//
+//	static long padovan(int n) {
+//		if (dp[n] != 0) {
+//			return dp[n];
+//		}
+//		return dp[n] = padovan(n-1) + padovan(n-5);
+//	}
+//}
