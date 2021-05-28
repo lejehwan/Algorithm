386e60486d0813b6495bf9dc069e9b257c34cad6
lehehwan <rdsa257@naver.com>
2021-05-28 20:27:55 +0900

    이분탐색

--- Algorithm/src/baekjoon이분탐색/수찾기.java
+++ Algorithm/src/baekjoon이분탐색/수찾기.java
@@ -38,3 +38,34 @@ public class 수찾기 {
 		sc.close();
 	}
 }
+
+// 이분탐색 내장 함수 사용(Arrays.binarySearch)
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		StringBuilder sb = new StringBuilder();
+//		int n = sc.nextInt();
+//		int[] arr = new int[n];
+//		for (int i = 0; i < arr.length; i++) {
+//			arr[i] = sc.nextInt();
+//		}
+//		int m = sc.nextInt();
+//		int[] check = new int[m];
+//		for (int i = 0; i < check.length; i++) {
+//			check[i] = sc.nextInt();
+//		}
+//		Arrays.sort(arr);
+//		for (int i = 0; i < check.length; i++) {
+//			int temp = 0;
+//			temp = Arrays.binarySearch(arr,check[i]);
+//			if (temp >= 0) {
+//				System.out.println(1);
+//			} else {
+//				System.out.println(0);
+//			}
+//		}
+//		sc.close();
+//	}
+//}
+
+
