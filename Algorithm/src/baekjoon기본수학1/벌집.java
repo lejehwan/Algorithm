5f85d770faf249845be2fd3305c25931ec09b0d7
lehehwan <rdsa257@naver.com>
2021-06-02 17:49:18 +0900

    기본수학1

---
+++ Algorithm/src/baekjoon기본수학1/벌집.java
@@ -0,0 +1,23 @@
+package baekjoon기본수학1;
+
+import java.util.Scanner;
+
+public class 벌집 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i = 0;
+		int j = 0;
+		while (true) {
+			int temp = 6*i;
+			if (n <= temp+1) {
+				break;
+			}else {
+				j++;
+				i += j;
+			}
+		}
+		System.out.println(j+1);
+		sc.close();
+	}
+}
---
+++ Algorithm/src/baekjoon기본수학1/손익분기점.java
@@ -0,0 +1,25 @@
+package baekjoon기본수학1;
+
+import java.util.Scanner;
+
+public class 손익분기점 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int count = 0;
+		while (true) {
+			if (B >= C) {
+				System.out.println("-1");
+				break;
+			} else if (A >= (C - B) * count) {
+				count++;
+			} else {
+				System.out.println(count);
+				break;
+			}
+		}
+		sc.close();
+	}
+}
