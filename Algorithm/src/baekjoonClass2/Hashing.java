4f9f9b39ed9640ae0b3af67ef38ca9e00ae29e26
lehehwan <rdsa257@naver.com>
2021-06-06 08:23:11 +0900

    Class2

---
+++ Algorithm/src/baekjoonClass2/Hashing.java
@@ -0,0 +1,25 @@
+package baekjoonClass2;
+
+import java.util.Scanner;
+
+// 최대 L이 49 이므로 Math.pow()사용시 초과
+// 31을 곱하고 mod로 나누어 준 값을 원래 값에 곱하는 방식으로 줄여야 함
+
+public class Hashing {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = sc.next();
+		int mod = 1234567891;
+		long sum = 0;
+		long r = 1;
+		for (int i = 0; i < n; i++) {
+			sum += (str.charAt(i) - 96) * r;
+			sum %= mod;
+			r *= 31;
+			r %= mod;
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+}
