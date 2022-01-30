f6dc4bb2a599d99f306b3934008072b97ec45a9b
lejehwan <rdsa2577@gmail.com>
2022-01-30 11:58:22 +0900

    Level1

--- Algorithm/src/programmersLevel1/숫자문자열과영단어.java
+++ Algorithm/src/programmersLevel1/숫자문자열과영단어.java
@@ -1,31 +1,5 @@
 package programmersLevel1;
 
-public class 숫자문자열과영단어 { 
- 
-	public static void main(String[] args) {
-		String s = "123";// one4seveneight 23four5six7 2three45sixseven 123
-		String[] englishWord = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-		String answer = "";
-		String[] ssplit = s.split("");
-		String temp = "";
-//		for (int i = 0; i < ssplit.length; i++) { 
-//			try {
-//				Integer.parseInt(ssplit[i]);  
-//				answer += ssplit[i];d
-//			} catch (Exception e) {
-//				temp += ssplit[i];
-//			}
-//			for (int j = 0; j < englishWord.length; j++) {
-//				if (temp.equals(englishWord[j])) {
-//					answer += String.valueOf(j);
-//					temp = "";
-//				}
-//			}
-//		}
-
-		System.out.println(s);
-	}
-}
 
 class 숫자문자열과영단어Solution1 {
 
