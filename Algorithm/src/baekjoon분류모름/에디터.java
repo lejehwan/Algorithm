8cf1a57c48ed79c37b05626e5701fd4e94436edb
lejehwan <rdsa2577@gmail.com>
2022-02-04 15:04:20 +0900

    스택

---
+++ Algorithm/src/baekjoon분류모름/에디터.java
@@ -0,0 +1,39 @@
+package baekjoon분류모름;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 에디터 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String[] str = br.readLine().split("");
+		int n = Integer.parseInt(br.readLine());
+		Stack<String> stack1 = new Stack<String>();
+		Stack<String> stack2 = new Stack<String>();
+		for (int i = 0; i < str.length; i++) {
+			stack1.push(str[i]);
+		}
+		for (int i = 0; i < n; i++) {
+			String[] command = br.readLine().split(" ");
+			if (command[0].equals("P")) {
+				stack1.push(command[1]);
+			} else if (command[0].equals("B") && !stack1.empty()) {
+				stack1.pop();
+			} else if (command[0].equals("L") && !stack1.empty()) {
+				stack2.push(stack1.pop());
+			} else if (command[0].equals("D") && !stack2.empty()) {
+				stack1.push(stack2.pop());
+			}
+		}
+		while (!stack1.empty()) {
+			stack2.push(stack1.pop());
+		}
+		while (!stack2.empty()) {
+			sb.append(stack2.pop());
+		}
+		System.out.println(sb.toString());
+	}
+}
