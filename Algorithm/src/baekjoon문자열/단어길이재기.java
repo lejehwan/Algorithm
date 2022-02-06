75e4f5e67f35988cecb035de570cdc242b745700
lejehwan <rdsa2577@gmail.com>
2022-02-06 11:48:55 +0900

    문자열

---
+++ Algorithm/src/baekjoon문자열/단어길이재기.java
@@ -0,0 +1,12 @@
+package baekjoon문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 단어길이재기 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+	}
+}
