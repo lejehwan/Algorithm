938a286bea2b2a37ba48218184b0ace7eb6b2ddd
lejehwan <rdsa2577@gmail.com>
2022-02-27 11:15:40 +0900

    스택

--- Algorithm/src/baekjoon스택/오큰수.java
+++ Algorithm/src/baekjoon스택/오큰수.java
@@ -1,37 +1,42 @@
 package baekjoon스택;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 public class 오큰수 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Stack<Integer> stack = new Stack<Integer>();
-		int n = sc.nextInt();
-		int[] seq = new int[n];
-		for (int i = 0; i < n; i++) {
-			seq[i] = sc.nextInt();
-		}
-		for (int i = 0; i < seq.length; i++) {
-			// 스택이 비어있지 않고, 현재원소(seq[i]) 스택의 맨 위가 가리키는 값보다 큰 경우
-			while (!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
-				// 스택의 맨 위가 가리키는 값을 지우고 현재원소(seq[i])에 넣음
-				seq[stack.pop()] = seq[i];
-			}
-			// 위 조건에 부합하지 않다면 스택에 해당 인덱스 삽입
-			stack.push(i);
-		}
-		// 마지막에 스택에 값이 남아있으면
-		while (!stack.isEmpty()) {
-			// 스택의 값이 가리키는 seq배열의 값을 -1로 변경
-			seq[stack.pop()] = -1;
-		}
-		// 각각을 출력하면 시간초과 => 한번에 출력
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		for (int answer : seq) {
-			sb.append(answer + " ");
+		Stack<Integer> stack1 = new Stack<Integer>();
+		Stack<Integer> stack2 = new Stack<Integer>();
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<Integer>();
+		String[] input = br.readLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			stack1.add(Integer.parseInt(input[i]));
 		}
-		System.out.println(sb.toString());
-		sc.close();
+		stack2.add(stack1.pop());
+		list.add(-1);
+		while(!stack1.empty()) {
+			if (stack2.empty()) {
+				stack2.add(stack1.pop());
+				list.add(-1);
+			}else if (stack1.peek() < stack2.peek()) {
+				list.add(stack2.peek());
+				stack2.add(stack1.pop());
+			}else {
+				stack2.pop();
+			}
+		}
+		Collections.reverse(list);
+		for (Integer val : list) {
+			sb.append(val + " ");
+		}
+		System.out.println(sb.toString().trim());
 	}
 }
